5d709ced368f5a46e034405879c27a8f109dbe6b
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-16 22:37:51 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -22,7 +22,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        :steven => {
+          :name => "Steven",
+          :age => "30",
+          :attitude => 
+        },
+        :nurse => {}
       ]
    }
 }
